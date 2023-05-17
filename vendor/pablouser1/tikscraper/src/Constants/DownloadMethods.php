3800683f64c179f062c82ad0b4abf0b673bc4ee8
Ayman <56372390+aymanoid@users.noreply.github.com>
2023-05-17 05:09:41 +0100

    add gitignore

--- vendor/pablouser1/tikscraper/src/Constants/DownloadMethods.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace TikScraper\Constants;
-
-abstract class DownloadMethods {
-    const DEFAULT = 'default';
-    const TTDOWN = 'ttdown';
-}
--- vendor/pablouser1/tikscraper/src/Interfaces/DownloaderInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace TikScraper\Interfaces;
-
-interface DownloaderInterface {
-    public function watermark(string $payload);
-    public function noWatermark(string $payload);
-}
